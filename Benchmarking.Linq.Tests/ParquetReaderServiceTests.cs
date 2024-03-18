fc967e69dc12c8fe442cc17a68e24fde3241303b
Brady Kelly <brady@bradykelly.net>
2024-03-18 10:09:07 +0200

    New test project

---
+++ Benchmarking.Linq.Tests/ParquetReaderServiceTests.cs
@@ -0,0 +1,17 @@
+namespace Benchmarking.Linq.Tests;
+
+public class ParquetReaderServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        // Method intentionally left empty until I have some benchmarks running.
+    }
+
+    [Test]
+    public void Test1()
+    {
+        // Test intentionally passes until I have some benchmarks running.
+        Assert.Pass();
+    }
+}
